5a3b5d0bec23a098083f55b5c0a90b51c941320f
Lukas Felano Nayoan <36601113+lukasfelano@users.noreply.github.com>
2018-05-13 20:16:23 +0700

    pemberian warna pada tiap sisi
    pemberian warna dilakukan saat di construtornya dadu dipanggil ... agar tiap sisi berbeda maka aku berikan array sehingga bisa nampung lebih dari 1 warna (pendapatku)
    
    kurang nambah fungsi getWarnaSisi() karena tidak tau isinya bagaimana

--- Dadu/Dadu.java
+++ Dadu/Dadu.java
@@ -4,20 +4,15 @@ import java.awt.Color;
 public final class Dadu {
   public static final int DEFAULT_SISI = 6;
 
-  private static Random randGenerator = new Random();
+  private Random randGenerator = new Random();
 
   private final int jumSisi;
   private int hasil;
+  private Random r;
+  private Random g;
+  private Random b;
   
-  private Color color;
-  
-  public Color getColor() {
-        return color;
-  }
-    
-  public void setColor(Color color) {
-        this.color = color;
-    }
+  private Color[] color;
   
   public Dadu() {
     this(DEFAULT_SISI);
@@ -25,9 +20,28 @@ public final class Dadu {
 
   public Dadu(int numSides) {
     this.jumSisi = numSides;
+    color =  new Color[jumSisi];
+    
+    for(int i=0 ;i<jumSisi;i++){ //perulangan untuk memberi warna pada tiap sisi
+        r = new Random();
+        g = new Random();
+        b = new Random();
+        
+        color[i] = new Color(r.nextInt(256),g.nextInt(256),b.nextInt(256)); //memberikan warna sisi/tampungan warna
+        
+        if(i>0){ //untuk melakukan pengecekan apabila randomnya sama dengan yang sudah ditampung
+            for(int j=i;j >0;j--){
+                if(color[i] == color[i-j]){
+                    color[i] = null;
+                    i--;
+                }
+            }
+        }
+    }
   }
 
   public int lempar() {
+    
     hasil = randGenerator.nextInt(jumSisi) + 1;
 
     return hasil;
